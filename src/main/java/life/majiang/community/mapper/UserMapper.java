c2321e9faf8ed73d4ce5ee6deb7cda7f139e5ab4
ra13806845602 <787499878@qq.com>
2020-04-21 19:34:03 +0800

    keep login

--- src/main/java/life/majiang/community/mapper/UserMapper.java
+++ src/main/java/life/majiang/community/mapper/UserMapper.java
@@ -9,4 +9,7 @@ import org.apache.ibatis.annotations.Select;
 public interface UserMapper {
     @Insert("insert into user(account_id,name,token,gmt_create,gmt_modified) values(#{accountId},#{name},#{token},#{gmtCreate},#{gmtModified})")
     public void insert(User user);
+
+    @Select("select * from user where token=#{token}")
+    User findByToken(String token);
 }
