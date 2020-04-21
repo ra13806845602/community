d1a9d5efeb20c4e1466f9b904b7bab4739e88843
unknown <787499878@qq.com>
2020-04-21 15:51:49 +0800

    init repo

---
+++ src/test/java/life/majiang/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package life.majiang.community;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CommunityApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
